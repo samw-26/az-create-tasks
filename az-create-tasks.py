271306bd3700643cfbb85e567a1fdd77162b27af
Samuel Wilton <samuel.wilton.03@gmail.com>
2025-11-22 23:24:34 -0500

    Substitute placeholders

--- az-create-tasks.py
+++ az-create-tasks.py
@@ -2,15 +2,59 @@ import yaml
 import argparse
 import re
 
+
+class TaskCreator:
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
+        self.template_file = self.args.template_file
+        self.values = self.args.values
+        if self.args.set is not None:
+            self.set_vars = create_var_dict(self.args.set)
+        self.substitute_placeholders()
+
+    def substitute_placeholders(self):
+        for task in self.template_file['tasks']:
+            for key, value in task.items():
+                task[key] = re.sub(
+                    r'\$((?P<index>[0-9]+)|(?P<key>\w+))',
+                    self.get_sub_value,
+                    value
+                )
+
+    def get_sub_value(self, placeholder: re.Match) -> str:
+        placeholder_index = placeholder.group('index')
+        key = placeholder.group('key')
+        index = (
+            None if placeholder_index is None
+            else int(placeholder_index) - 1
+        )
+        values_length = len(self.values)
+
+        if (
+            index is not None
+            and values_length > 0
+            and index < values_length
+            and index >= 0
+        ):
+            return self.values[index]
+
+        if key and self.set_vars is not None and key in self.set_vars:
+            return self.set_vars[key]
+
+        return placeholder.group()
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Create child tasks of work items')
-    parser.add_argument('template_file', help='YAML file defining child tasks.', type=parse_yaml)
+    parser = argparse.ArgumentParser(
+        description='Create child tasks of work items')
+    parser.add_argument(
+        'template_file', help='YAML file defining child tasks.', type=parse_yaml)
     parser.add_argument(
         '--set',
         action='append',
         metavar='KEY=VALUE',
-        help='Define custom variables. In the template file, reference them with $KEY',
-        type=convert_to_dict
+        help='Define custom variables. In the template file, reference them with $KEY. Names cannot start with a digit.',
+        type=parse_set_vars
     )
     parser.add_argument(
         'values',
@@ -18,9 +62,11 @@ def main():
         nargs='*'
     )
     args = parser.parse_args()
+    task_creator = TaskCreator(args)
     print(args.template_file, args.set, args.values)
 
-def parse_yaml(file_name):
+
+def parse_yaml(file_name: str):
     VALID_TASK_PROPERTIES = ['name', 'assigned']
     try:
         with open(file_name, 'r') as f:
@@ -33,23 +79,40 @@ def parse_yaml(file_name):
             raise argparse.ArgumentTypeError('tasks is not an array')
         for i, task in enumerate(result['tasks']):
             task_number = i + 1
+            has_name = False
             for key in task:
                 if key not in VALID_TASK_PROPERTIES:
-                    raise argparse.ArgumentTypeError(f'Invalid key in Task {task_number}: {key}')
-            if 'name' not in task:
-                raise argparse.ArgumentTypeError(f'Task {task_number} does not have name')
-            
+                    raise argparse.ArgumentTypeError(
+                        f'Invalid key in Task {task_number}: {key}')
+                if key == 'name':
+                    has_name = True
+
+            if not has_name:
+                raise argparse.ArgumentTypeError(
+                    f'Task {task_number} does not have name')
+
     except FileNotFoundError:
         raise argparse.ArgumentTypeError('File not found')
     return result
 
-def convert_to_dict(var):
+
+def parse_set_vars(var: str):
+    if re.match(r'^\d', var) is not None:
+        raise argparse.ArgumentTypeError(
+            f'Invalid key name, key cannot start with digit: {var}')
     if '=' not in var:
-        raise argparse.ArgumentTypeError(f'Invalid format, expected: "KEY=VALUE", received: "{var}"')
-    value_dict = {}
+        raise argparse.ArgumentTypeError(
+            f'Invalid format, expected: "KEY=VALUE", received: "{var}"')
     key, value = var.split('=', 1)
-    value_dict[key] = value
-    return value_dict
+    return key, value
+
+
+def create_var_dict(set_vars):
+    vars = {}
+    for entry in set_vars:
+        vars[entry[0]] = entry[1]
+    return vars
+
 
 if __name__ == '__main__':
     main()
